d2ba6109fecbaeee0001ebc799a0a7d27b4cf499
mustafa <mustafa.sadriwala@utdallas.edu>
2019-01-26 23:47:49 -0600

    hyperlink color change

--- dist/main.bundle.js
+++ dist/main.bundle.js
@@ -59,7 +59,7 @@
 /******/ 	
 /******/ 	
 /******/ 	var hotApplyOnUpdate = true;
-/******/ 	var hotCurrentHash = "1a73595e96bfce044cc5"; // eslint-disable-line no-unused-vars
+/******/ 	var hotCurrentHash = "6401be8dcd369df1e827"; // eslint-disable-line no-unused-vars
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule; // eslint-disable-line no-unused-vars
 /******/ 	var hotCurrentParents = []; // eslint-disable-line no-unused-vars
